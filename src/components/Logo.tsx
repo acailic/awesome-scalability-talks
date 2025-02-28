d972d1979be09e6fc8f15ef6dcc150b8bf340f7e
aleksandarilic <a.ilic@vegait.rs>
2025-02-28 10:16:47 +0100

    update

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,9 @@
+import { getAssetPath } from '../utils/paths';
+
 export default function Logo() {
   return (
     <a href="/" className="logo">
-      <img src="../src/assets/Heart.png" alt="logo" />
+      <img src={getAssetPath('/src/assets/Heart.png')} alt="logo" />
     </a>
   );
 }
